27ea553daca9892936884e757e195e706e14c570
sovonthy <sovonthy.leng069@gmail.com>
2020-06-21 00:00:06 +0700

    delete student

--- resources/views/students/followUp.blade.php
+++ resources/views/students/followUp.blade.php
@@ -44,7 +44,15 @@
                                 <td>{{$student->firstname}}.{{$student->lastname}}</td>
                                 <td>{{$student->class}}</td>
                                 <td >
-                                <a href="{{route('students.destroy', $student->id)}}"><span class="material-icons text-danger">person_add_disabled</span></a>&nbsp; | &nbsp;
+
+                                <form method="POST" action="{{route('students.destroy', $student->id)}}">
+                                      @csrf
+                                      @method('DELETE')
+
+                                     <button type="submit"><span  class="material-icons text-danger">person_add_disabled</span></button>
+                                    </form>
+
+                              &nbsp; | &nbsp;
                                 <a href="{{route('students.edit', $student->id)}}"><span  class="material-icons text-success">how_to_reg </span></a>
                                 </td>
                                 @endif
